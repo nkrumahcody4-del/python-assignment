deaba43e62ecaedbc9cdbde9cb308b0060795158
Kofi.cr24 <nkrumahcody4@gmail.com>
2025-12-10 16:41:38 +0000

    Create Basics of Python.py

---
+++ Basics of Python.py
@@ -0,0 +1,23 @@
+# 1
+Playlist  = ["Stopper ","Nobody","In this","Fair","STOP"]
+
+# 2
+top_artists = ("kofi mole", "Shatta wale", "Kidi", "Sarkodie",)
+
+# 3
+my_music = [Playlist, top_artists]
+
+# 4
+print(len(my_music))
+
+# 5
+print(Playlist[2])
+
+# 6
+Playlist[4] = "Perfect"
+
+#7
+Playlist.append("Feeling You")
+
+# 8
+print(Playlist)
